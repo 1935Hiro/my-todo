79f1d40ab3bacd56d1d2d27c4ed3293d91014ee6
1935Hiro <espresso.spacewood@gmail.com>
2022-08-21 12:07:00 +0900

    install laravel

---
+++ laravel/app/Http/Requests/CreateTask.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTask extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:100',
+            'due_date' => 'required|date|after_or_equal:today',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル',
+            'due_date' => '期限日',
+        ];
+    }
+
+    public function messages()
+    {
+        return[
+            'due_date.after_or_equal' => ':attribute には今日以降の日付を入力してください。',
+        ];
+    }
+}
